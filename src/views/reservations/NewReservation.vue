2d8273ad2d51374bc7d29f63f73ee24da09d029f
Jonathan Paul Moyonero Espinoza <moyonero@gmail.com>
2021-05-23 23:48:32 +0200

    ROOM-174: Reservas actualizadas despues de registrar una

--- src/views/reservations/NewReservation.vue
+++ src/views/reservations/NewReservation.vue
@@ -190,9 +190,32 @@ export default {
           .then(response => {
             this.showSuccessModal(response.data)
             this.resetForm()
+            axios
+                .get('https://backend-dev.roomscape.es/reservation/list?escapeRoomId=' + this.$route.params.id)
+                .then(response => {
+                  this.reservas = response.data
+                })
           })
           .catch(err => {
             this.showWarningModal(err.response.data)
+          })
+
+      let createReservation =  axios
+          .post('https://backend-dev.roomscape.es/reservation/create', reservation)
+          .then(response => {
+            this.showSuccessModal(response.data)
+            this.resetForm()
+          })
+          .catch(err => {
+            this.showWarningModal(err.response.data)
+          })
+      Promise.all([createReservation])
+          .then(() => {
+            axios
+                .get('https://backend-dev.roomscape.es/reservation/list?escapeRoomId=' + this.$route.params.id)
+                .then(response => {
+                  this.reservas = response.data
+                })
           })
     },
     showSuccessModal(reservation) {
