149921de27a4fb0e72a826ddb43cd2160ef11e1e
Juan <43722131+JuanTecedor@users.noreply.github.com>
2021-05-24 20:10:28 +0200

    ROOM-169 :)

--- tests/unit/views/login/UserLogin.spec.js
+++ tests/unit/views/login/UserLogin.spec.js
@@ -9,7 +9,7 @@ describe('Elementos HTML de la vista de login usuario', () => {
 
     const $cookies = {
         get: function () {
-            return "session_id"
+            return undefined
         }
     };
     let wrapper = mount(UserLogin, {
@@ -20,8 +20,7 @@ describe('Elementos HTML de la vista de login usuario', () => {
     });
 
     it('Validación del título de la página', async () => {
-        console.log(wrapper.html())
-        // expect(wrapper.find('#title').text()).toEqual("Login")
+        expect(wrapper.find('#title').text()).toEqual("Login")
     })
 
     it('Validación del formulario', async  () => {
@@ -29,11 +28,30 @@ describe('Elementos HTML de la vista de login usuario', () => {
         let passwd = wrapper.find('#input-group-password')
         expect(passwd.text()).toEqual("Contraseña:min. 8 caracteres, al menos 1 letra y 1 número")
         expect(passwd.find('#input-password').props().type).toEqual('password')
-        expect(wrapper.find('#input-group-dni').text()).toEqual("DNI:")
+        expect(wrapper.find('#input-group-user').text()).toEqual("Usuario:")
     })
 
     it('Validacion botones', async () => {
-        expect(wrapper.find('.button-success').isVisible()).toEqual(true)
-        expect(wrapper.find('.button-cancel').isVisible()).toEqual(true)
+        expect(wrapper.find('.button-success-login').isVisible()).toEqual(true)
+        expect(wrapper.find('.button-cancel-login').isVisible()).toEqual(true)
+    })
+})
+
+describe('Prueba redirección', () => {
+
+    const $cookies = {
+        get: function () {
+            return 'easter egg'
+        }
+    };
+    let wrapper = mount(UserLogin, {
+        localVue,
+        mocks: {
+            $cookies
+        }
+    });
+
+    it('Si estamos logeados no mostramos formulario de login', async () => {
+        expect(wrapper.html()).toEqual('')
     })
 })
